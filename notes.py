b75d6c86133b8493a95344f5ad29bfe1907e4c80
dillonrowan <dhr14@txstate.edu>
2016-10-11 14:44:36 -0500

    changed notes.py

--- notes.py
+++ notes.py
@@ -1,44 +1,23 @@
 import itertools
 import datetime
+from . import search_database
 
-class slamenote:
-    #Creates incremented id for object. To use, write object.id
-    id_generator = itertools.count(0) #starts first object at id 1
-    def __init__(self):
-        self.id = next(self.id_generator) #increments id
-        self.createdDate = datetime.date.today() #object has day it was made
-  
-    def setAuthor(self, name):
-        self.author = name
-    def getAuthor(self):
-        return self.author
+class note:
+    pass
     
-    def setSchool(self, school):
-        self.school = school
-    def getSchool(self):
-        return self.school
-    
-    def setMajor(self, major):
-        self.major = major
-    def getMajor(self):
-        return self.major
-    
-    def setCourse(self, course):
-        self.course = course
-    def getCourse(self):
-        return self.course
 
-    def setInstr(self, instr):
-        self.instr = instr
-    def getInstr(self):
-        return self.instr
-
-    def setUnit(self, unit):
-        self.unit = unit
-    def getUnit(self):
-        return self.unit
-
-    def setSection(self, section):
-        self.section = section
-    def getSection(self):
-        return self.section
+def loadNote(id):
+    newNote = note()
+    note_data = search_database.findNoteByID(id)
+    newNote.id = id
+    newNote.author = note_data['author']
+    newNote.school = note_data['school']
+    newNote.major = note_data['major']
+    newNote.course = note_data['course']
+    newNote.instr = note_data['instr']
+    newNote.unit = note_data['unit']
+    newNote.section = note_data['section']
+    newNote.note_date = note_data['note_date']
+    return newNote
+        
+   
