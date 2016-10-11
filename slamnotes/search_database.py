486bbf7f994fd8dd37c5fda2d260ebd4f6b0fd00
Tristan Thielemann <tristan.thielemann@gmail.com>
2016-10-11 15:05:47 -0500

    adding dictionary return

--- slamnotes/search_database.py
+++ slamnotes/search_database.py
@@ -4,10 +4,16 @@ import MySQLdb as mariadb
 def findNoteByID(num):
 	mariadb_connection = mariadb.connect(user='root', passwd='4Rct!c', db = 'primary_db')
 	cursor = mariadb_connection.cursor()
-	query = ("SELECT note_body, created_by, note_ID, note_date FROM notes "
+	query = ("SELECT note_body, created_by, note_date FROM notes "
 		"WHERE note_ID LIKE '%s'" %(num))
 	cursor.execute(query)
-	for(note_body, created_by, note_ID, note_date) in cursor:
-		print("%s, %s, %s, %s " % (note_body.decode('utf-8'), created_by, note_ID, note_date))
+	for(note_body, created_by, note_date) in cursor:
+		note_dict = {
+			'body' = note_body,
+			'author' = created_by,
+			'note_date' = note_date,
+			}
+		print("%s, %s, %s " % (note_body.decode('utf-8'), created_by, note_date))
 	cursor.close()
 	mariadb_connection.close()
+	return note_dict
