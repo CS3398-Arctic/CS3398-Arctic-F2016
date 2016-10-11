52fa0ef03a7f6a1579a85b5e52286e50137ee960
dillonrowan <dhr14@txstate.edu>
2016-10-11 16:33:10 -0500

    commented all atributes except body, author, and date

--- slamnotes/notes.py
+++ slamnotes/notes.py
@@ -12,12 +12,12 @@ def loadNote(id):
     newNote.id = id
     newNote.body = note_data['body']
     newNote.author = note_data['author']
-    newNote.school = note_data['school']
-    newNote.major = note_data['major']
-    newNote.course = note_data['course']
-    newNote.instr = note_data['instr']
-    newNote.unit = note_data['unit']
-    newNote.section = note_data['section']
+    #newNote.school = note_data['school']
+    #newNote.major = note_data['major']
+    #newNote.course = note_data['course']
+    #newNote.instr = note_data['instr']
+    #newNote.unit = note_data['unit']
+    #newNote.section = note_data['section']
     newNote.note_date = note_data['note_date']
     return newNote
         
