7b4fe8154e5ea6a269aaba8d7b53f96fa733674f
TristanThielemann <tristan.thielemann@gmail.com>
2016-10-16 20:44:49 -0500

    add rudementary view functions

--- slamnotes/urls.py
+++ slamnotes/urls.py
@@ -22,6 +22,8 @@ from . import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
+	url(r'^$', views.view_time)
+	url(r'^$', views.view_text_body, )
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 ]
