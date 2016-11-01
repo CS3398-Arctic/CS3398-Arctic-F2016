42a4d4357702b7ac0ed823a73d82b6a59506169a
Peter Cassetta <peter@cassetta.info>
2016-10-31 23:19:20 -0500

    account creation working now, removed unneeded views and urls, modified CSS and deleted templates/registration

--- slamnotes/urls.py
+++ slamnotes/urls.py
@@ -23,12 +23,7 @@ from . import views
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
     url(r'^$', views.index, name='index'),
-    url(r'^view-time$', views.view_time),
     url(r'^channel/1$', views.channel, name='channel'),
-    url(r'^user-test$', views.user_test, name='user-test'),
-    url(r'^create-account$', views.create_account, name='create-account'),
-    url(r'^login', views.login, name='login'),
-    url(r'^account-created$', views.account_created, name='account-created'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- slamnotes/views.py
+++ slamnotes/views.py
@@ -3,10 +3,10 @@
 Several function-based views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/views/
 """
-import datetime
+
+# import datetime
 
 from django.contrib.auth.models import User
-from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Note, NoteForm, SignupForm, LoginForm
@@ -14,15 +14,17 @@ from .models import Note, NoteForm, SignupForm, LoginForm
 
 def index(request):
     """Home page view"""
+    account_created = False
+
     if request.method == 'POST':
         form_login = LoginForm(request.POST, request.FILES)
         form_signup = SignupForm(request.POST, request.FILES)
-        if form_login.is_valid():
+
+        if "login-form" in request.POST and form_login.is_valid():
             User.objects.login(**form_login.cleaned_data)
-            return render(request, 'login_successful.html')
-        elif form_signup.is_valid():
+        elif "signup-form" in request.POST and form_signup.is_valid():
             User.objects.create_user(**form_signup.cleaned_data)
-            return render(request, 'account_created.html')
+            account_created = True
     else:
         form_login = LoginForm()
         form_signup = SignupForm()
@@ -31,23 +33,8 @@ def index(request):
                   {
                       'form_login': form_login,
                       'form_signup': form_signup,
+                      'account_created': account_created,
                   })
-
-
-def view_time(request):
-    now = datetime.datetime.now()
-    html = "<html><body>Current Time: %s.</body></html>" % now
-    return HttpResponse(html)
-
-
-def text_body_view(request):
-    most_recent_note = Note.object.order_by('-created_date')[:5]
-    output = ', '.join([n.body_text for n in most_recent_note])
-    return HttpResponse(output)
-
-
-def view_note_id(request, note_id):
-    return HttpResponse("Note ID: %s." % note_id)
 
 
 def channel(request):
@@ -67,43 +54,3 @@ def channel(request):
                       'notes': all_notes,
                       'user': request.user,
                   })
-
-
-def user_test(request):
-    """user test page view"""
-    return render(request, 'login.html')
-
-
-def create_account(request):
-    """account creation page"""
-    if request.method == 'POST':
-        form = SignupForm(request.POST, request.FILES)
-        if form.is_valid():
-            User.objects.create_user(**form.cleaned_data)
-            return render(request, 'account_created.html')
-    else:
-        form = SignupForm()
-    return render(request, 'create_account.html',
-                  {
-                      'form': form,
-                  })
-
-
-def login(request):
-    """login page"""
-    if request.method == 'POST':
-        form = LoginForm(request.POST, request.FILES)
-        if form.is_valid():
-            User.objects.login(**form.cleaned_data)
-            return render(request, 'login_successful.html')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html',
-                  {
-                      'form': form,
-                  })
-
-
-def account_created(request):
-    """account created test page"""
-    return render(request, 'account_created.html')
