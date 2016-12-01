2812a209dd1a542962dc47ed3e5080d36ba6ba45
dillonrowan <dhr14@txstate.edu>
2016-11-30 18:22:57 -0600

    this is the url that guided us in utilizing this rating system: https://github.com/wildfish/django-star-ratings

--- slamnotes/settings.py
+++ slamnotes/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'slamnotes',
+    'star_ratings',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                #'django.core.context_processors.request',
             ],
         },
     },
--- slamnotes/urls.py
+++ slamnotes/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.conf.urls import include, url
+
 
 from . import views
 
@@ -29,4 +31,6 @@ urlpatterns = [
     url(r'^ajax$', views.ajax, name='ajax'),
     url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',
         content_type='text/plain')),
+    url(r'^ratings/', include('star_ratings.urls', namespace='ratings', app_name='ratings')),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
